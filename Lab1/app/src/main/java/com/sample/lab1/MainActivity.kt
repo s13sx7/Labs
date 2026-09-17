66c11ec5e872102b511560e2f339b0dfcc983334
s13sx7 <s13sx7@gmail.com>
2026-09-17 16:04:04 +0700

    try5

---
+++ Lab1/app/src/main/java/com/sample/lab1/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.sample.lab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sample.lab1.ui.theme.Lab1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun  DemoText(message: String, fontSize: Float){
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
+
+    ) //asdasdasdasd
+}
+
+
+
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float)-> Unit){
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..1000f,
+        value = sliderPosition,
+        onValueChange = onPositionChange
+    )
+}
+
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    var sliderPosition by remember { mutableFloatStateOf(20f) }
+    val handlePositionChange = { position : Float ->
+        sliderPosition = position
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+      DemoText(message = "Welcome to Compose", fontSize = sliderPosition)
+      Spacer(modifier = Modifier.height(150.dp))
+      DemoSlider(
+          sliderPosition = sliderPosition,
+          onPositionChange = handlePositionChange
+      )
+        Text(
+            style = MaterialTheme.typography.headlineMedium,
+            text = sliderPosition.toInt().toString() + "sp"
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DemoTextPreview() {
+    Lab1Theme() {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            DemoScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
